73c15ceaabc1f09d7284187edbd110bc12717e34
sarunkurup3 <sarunkurup3@gmail.com>
2018-10-02 20:18:00 +0530

    updated the class pageObjects

--- src/main/java/com/pageObjects/Page1.java
+++ src/main/java/com/pageObjects/Page1.java
@@ -2,4 +2,12 @@ package com.pageObjects;
 
 public class Page1 {
 
+	public static void display() {
+		System.out.println("New Code insertion");
+	}
+	
+	public static void main(String args[]) {
+		Page1.display();
+	}
+	
 }
