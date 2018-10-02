4562ea36095aa8706d147e6950c50fa8332a6d2c
sarunkurup3 <sarunkurup3@gmail.com>
2018-10-02 21:01:41 +0530

    new lines

--- src/main/java/com/pageObjects/Page3.java
+++ src/main/java/com/pageObjects/Page3.java
@@ -4,6 +4,7 @@ public class Page3 {
 
 	public static void main(String args[]) {
 		System.out.println("Page 3 MASTER CODE UPDATE");
+		System.out.println("deleted the above one -> New Changes sysout");
 	}
 	
 }
